3eb95a6549fdb8a55cd2da78746093b63cf9df5e
Benny Neugebauer <mail@bennycode.com>
2024-11-16 18:37:07 +0100

    refactor

--- src/converter/replaceRequire.ts
+++ src/converter/replaceRequire.ts
@@ -1,38 +1,45 @@
 import {SourceFile, SyntaxKind, VariableStatement} from 'ts-morph';
 
 export function replaceRequire(sourceFile: SourceFile, statement: VariableStatement) {
-  // Check if the initializer is a require call
+  // Get variable declaration
   const declaration = statement.getDeclarations()[0];
   if (!declaration) {
     return false;
   }
 
+  // Get call expression from variable declaration
   // @see https://github.com/dsherret/ts-morph/issues/682#issuecomment-520246214
   const initializer = declaration.getInitializerIfKind(SyntaxKind.CallExpression);
   if (!initializer) {
     return false;
   }
 
-  // Extract the argument passed to "require()" and use its value
-  const args = initializer.getArguments();
-  const moduleSpecifierNode = args[0];
-  if (!moduleSpecifierNode) {
+  // Verify that we have a "require" call
+  const identifier = initializer.getExpression().asKind(SyntaxKind.Identifier);
+  if (identifier?.getText() !== 'require') {
     return false;
   }
 
-  // Narrowing the Node and accessing its literal value
-  const moduleName = moduleSpecifierNode.asKind(SyntaxKind.StringLiteral)?.getLiteralValue();
-  if (!moduleName) {
+  // Extract the argument passed to "require" and use its value
+  const requireArguments = initializer.getArguments();
+  const packageName = requireArguments[0];
+  if (!packageName) {
+    return false;
+  }
+
+  // Narrowing the argument down to its literal value
+  const packageNameLiteral = packageName.asKind(SyntaxKind.StringLiteral);
+  if (!packageNameLiteral) {
     return false;
   }
 
   // Add import declaration
   sourceFile.addImportDeclaration({
     defaultImport: declaration.getName(),
-    moduleSpecifier: moduleName,
+    moduleSpecifier: packageNameLiteral.getLiteralValue(),
   });
 
-  // Remove the original require statement
+  // Remove the original "require" statement
   statement.remove();
   return true;
 }
