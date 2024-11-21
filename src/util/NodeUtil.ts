2a789f62d8ba4cb2cee637f53ce5923ea258354e
Benny Neugebauer <mail@bennycode.com>
2024-11-21 13:40:32 +0100

    chore: Add util to find comments without only line-breaks

--- src/util/NodeUtil.ts
+++ src/util/NodeUtil.ts
@@ -1,13 +1,18 @@
 import {Expression, Statement} from 'ts-morph';
 
 export class NodeUtil {
+  static hasOnlyLineBreaks(input: string) {
+    return /^[\r\n]*$/.test(input);
+  }
   static extractComment(input: Statement | Expression) {
     const statementWithComment = input.getFullText();
     const pureStatement = input.getText();
     const comment = statementWithComment.replace(pureStatement, '');
     const statement = statementWithComment.replace(comment, '');
+    const hasNoText = this.hasOnlyLineBreaks(comment) ? true : false;
     return {
       comment,
+      hasNoText,
       statement,
     };
   }
